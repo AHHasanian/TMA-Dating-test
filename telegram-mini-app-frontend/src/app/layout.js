d7701b732cf8246b29bf073bfd7e744d6f7da1ba
Amir Hossein Hasanian <hasanianamirhossein@gmail.com>
2026-08-25 22:55:35 +0330

    Initial Next.js Docker setup  On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   .gitignore
    	new file:   docker-compose.yml
    	new file:   telegram-mini-app-frontend/.dockerignore
    	new file:   telegram-mini-app-frontend/Dockerfile
    	new file:   telegram-mini-app-frontend/eslint.config.mjs
    	new file:   telegram-mini-app-frontend/jsconfig.json
    	new file:   telegram-mini-app-frontend/next.config.mjs
    	new file:   telegram-mini-app-frontend/package-lock.json
    	new file:   telegram-mini-app-frontend/package.json
    	new file:   telegram-mini-app-frontend/public/file.svg
    	new file:   telegram-mini-app-frontend/public/globe.svg
    	new file:   telegram-mini-app-frontend/public/next.svg
    	new file:   telegram-mini-app-frontend/public/vercel.svg
    	new file:   telegram-mini-app-frontend/public/window.svg
    	new file:   telegram-mini-app-frontend/src/app/favicon.ico
    	new file:   telegram-mini-app-frontend/src/app/globals.css
    	new file:   telegram-mini-app-frontend/src/app/layout.js
    	new file:   telegram-mini-app-frontend/src/app/page.js
    	new file:   telegram-mini-app-frontend/src/app/page.module.css

---
+++ telegram-mini-app-frontend/src/app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Telegram Mini App",
+  description: "Telegram Mini App",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ telegram-mini-app-frontend/src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  const appName = "Telegram Mini App see me 1599";
+
+  return (
+    <main>
+      <h1>{appName}</h1>
+      <p>Welcome to my application.</p>
+    </main>
+  );
+}
